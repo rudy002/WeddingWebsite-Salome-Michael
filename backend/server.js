fb1086d10d5b2a457a4a1baafbdd5645c6bcf1e7
rudy002 <rudyhaddad.job@gmail.com>
2024-07-31 14:13:53 +0300

    Update CORS configuration to include Netlify domain

--- backend/server.js
+++ backend/server.js
@@ -3,28 +3,29 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 require('dotenv').config();
 const router = require('./routes');
+const cors = require('cors');
+
+const allowedOrigins = ['http://localhost:3000', 'https://sage-lokum-3f7c33.netlify.app'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.options('*', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.status(200).send();
-});
-
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use('/api', router);
-
 
 // Endpoint pour récupérer des données depuis Airtable
 app.get('/api/data', async (req, res) => {
@@ -38,14 +39,12 @@ app.get('/api/data', async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Something went wrong' });
-  } 
+  }
 });
 
 // Route de test pour vérifier si le backend est connecté
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Backend is connected!' });
 });
-
-
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
